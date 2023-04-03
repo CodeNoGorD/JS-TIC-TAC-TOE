5bd0a6b6cf6f4a7f2b4817311933ed98ed22e3b4
Nahima697 <nahima.toumi697@gmail.com>
2023-04-03 16:45:32 +0200

    creation des classes

---
+++ JS/game.js
@@ -0,0 +1,13 @@
+"user strict";
+
+class Game {
+    name;
+    player;
+    round;
+    constructor(name, player, round) {
+    this.name = name;
+    this.avatar = player;
+    this.round = round;
+    }
+
+}
