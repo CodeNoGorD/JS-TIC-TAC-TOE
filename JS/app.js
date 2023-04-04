a00ab1d294754bf5f29202a398924438c703065f
CodeNoGorD <code.nogord@gmail.com>
2023-04-04 16:00:27 +0200

    formulaires modifs

--- JS/app.js
+++ JS/app.js
@@ -13,97 +13,6 @@ let bienvenue = document.querySelector('.bienvenue');
 console.log(gameTurn.value);
 
 
-class Game{
-    constructor(nbPlayers, player1, player2){
-        this.nbPlayers = nbPlayers;
-        this.gameOver = false;
-        this.player1 = player1;
-        this.player2 = player2;
-        this.nbParty = 1;
-    }
-    createGrid(vertical, horizontal){
-        let gridText = [];
-        for (let i = 0; i < vertical; i++){
-            let row = [];
-            for(let j = 0; j < horizontal; j++){
-                row.push(`[${i}][${j}]`);
-            }  
-            gridText.push(row);
-        }
-        // console.log(gridText);
-        return(gridText);
-    }
-    showGrid(tab){
-        let table = document.createElement('table');
-    
-        tab.forEach(line =>{
-            let tr = document.createElement('tr');
-            line.forEach( el => {
-                let td= document.createElement('td');
-                td.setAttribute('data', el);
-                let img = document.createElement('img');
-                img.setAttribute('src', './images/question.png');
-                td.appendChild(img);
-                tr.appendChild(td);
-            });
-            table.appendChild(tr);
-        });
-        table.addEventListener('click', (e) => {
-            let X = e.target.parentNode.getAttribute('data').slice(1, 2);
-            let Y = e.target.parentNode.getAttribute('data').slice(4, 5);
-
-                if(gameTurn.value % 2 != 0){
-                    e.target.setAttribute('src', './images/cross.png');
-                    e.target.parentNode.classList.add('cross');
-                    e.target.parentNode.style.pointerEvents = 'none';
-                    gameResult[X][Y] = 'cross';
-                    game.gameOver = verifResult();
-                    if(game.gameOver == true){
-                        containerAffichage.style.display = 'flex';
-                        affichage.innerText = `Bravo ${player2}, vous avez GAGNE !`;
-                        // console.table(gameResult);
-                    } else if (gameTurn.value > 7 && game.gameOver == false){
-                        containerAffichage.style.display = 'flex';
-                        affichage.innerText = `PERDU vous pouvez recommencer !`;
-                    };
-                    console.log(gameTurn.value);
-                    console.log(game.gameOver);
-                }
-                if(gameTurn.value % 2 == 0){
-                    e.target.setAttribute('src', './images/circle.png');
-                    e.target.parentNode.classList.add('circle');
-                    e.target.parentNode.style.pointerEvents = 'none';
-                    gameResult[X][Y] = 'circle';
-                    game.gameOver = verifResult();
-                    if(game.gameOver == true){
-                        containerAffichage.style.display = 'flex';
-                        affichage.innerText = `Bravo ${player1}, vous avez GAGNE !`;
-                        // console.table(gameResult);
-                    } else if (gameTurn.value > 7 && game.gameOver == false){
-                        containerAffichage.style.display = 'flex';
-                        affichage.innerText = `PERDU vous pouvez recommencer !`;
-                    };
-                    console.log(gameTurn.value);
-                    console.log(game.gameOver);
-                }
-                gameTurn.value++;
-            });
-        return table;
-    }
-    restart(){
-        grid.innerHTML = '';
-        containerAffichage.style.display = 'none';
-        
-        game = new Game(2, player1, player2);
-        gameGrid = game.createGrid(3, 3);
-        gameResult = gameGrid.map( el => el);
-        table = game.showGrid(gameGrid);
-        grid.appendChild(table);
-        gameTurn.value = 0;
-        game.gameOver = false;
-        game.nbParty++;
-    }
-}
 
 
 //GAMEPLAY
@@ -129,31 +38,30 @@ function verifResult(){
 
 // EVENT LISTENERS
 
-// btnValid.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     player1 = document.querySelector('#player1').value;
-//     player2 = document.querySelector('#player2').value;
-//     console.log(player1);
-//     console.log(player2);
-//     bienvenue.textContent = 'Vous pouvez commencer...';
-// });
 
 btnRestart.addEventListener('click', game.restart);
 
-
+grid.style.display="none";
 btnValid.addEventListener('click', (e) => {
     e.preventDefault();
     player1 = document.querySelector('#player1').value;
     player2 = document.querySelector('#player2').value;
     
 
-    if (player1 === "") {
-        bienvenue.textContent = 'Le joueur N°1 doit renseigner un nom valide !';
-    } else if(player2 === "") {
-        bienvenue.textContent = 'Le joueur N°2 doit renseigner un nom valide !';
-    } else {
-        bienvenue.textContent = 'Vous pouvez commencer...';
-        grid.appendChild(table).style.display="block";
+    try{
+        if (player1 === "" && player2 === "") {
+            throw new Error('Veuillez remplir les champs !');
+        } else if(player1 === "") {
+            throw new Error('Le joueur N°1 doit renseigner un nom valide !');
+        } else if(player2 === "") {
+            throw new Error('Le joueur N°2 doit renseigner un nom valide !');
+        } else {
+            bienvenue.textContent = 'Vous pouvez commencer...';
+            grid.style.display="grid";
+        }
+    }
+    catch (e){
+        bienvenue.textContent = e.message;
     }
 });
 
--- JS/game.js
+++ JS/game.js
@@ -1,13 +1,95 @@
 "user strict";
 
-class Game {
-    name;
-    player;
-    round;
-    constructor(name, player, round) {
-    this.name = name;
-    this.avatar = player;
-    this.round = round;
-    }
 
+
+class Game{
+    constructor(nbPlayers, player1, player2){
+        this.nbPlayers = nbPlayers;
+        this.gameOver = false;
+        this.player1 = player1;
+        this.player2 = player2;
+        this.nbParty = 1;
+    }
+    createGrid(vertical, horizontal){
+        let gridText = [];
+        for (let i = 0; i < vertical; i++){
+            let row = [];
+            for(let j = 0; j < horizontal; j++){
+                row.push(`[${i}][${j}]`);
+            }  
+            gridText.push(row);
+        }
+        // console.log(gridText);
+        return(gridText);
+    }
+    showGrid(tab){
+        let table = document.createElement('table');
+    
+        tab.forEach(line =>{
+            let tr = document.createElement('tr');
+            line.forEach( el => {
+                let td= document.createElement('td');
+                td.setAttribute('data', el);
+                let img = document.createElement('img');
+                img.setAttribute('src', './images/question.png');
+                td.appendChild(img);
+                tr.appendChild(td);
+            });
+            table.appendChild(tr);
+        });
+        table.addEventListener('click', (e) => {
+            let X = e.target.parentNode.getAttribute('data').slice(1, 2);
+            let Y = e.target.parentNode.getAttribute('data').slice(4, 5);
+
+                if(gameTurn.value % 2 != 0){
+                    e.target.setAttribute('src', './images/cross.png');
+                    e.target.parentNode.classList.add('cross');
+                    e.target.parentNode.style.pointerEvents = 'none';
+                    gameResult[X][Y] = 'cross';
+                    game.gameOver = verifResult();
+                    if(game.gameOver == true){
+                        containerAffichage.style.display = 'flex';
+                        affichage.innerText = `Bravo ${player2}, vous avez GAGNE !`;
+                        // console.table(gameResult);
+                    } else if (gameTurn.value > 7 && game.gameOver == false){
+                        containerAffichage.style.display = 'flex';
+                        affichage.innerText = `PERDU vous pouvez recommencer !`;
+                    };
+                    console.log(gameTurn.value);
+                    console.log(game.gameOver);
+                }
+                if(gameTurn.value % 2 == 0){
+                    e.target.setAttribute('src', './images/circle.png');
+                    e.target.parentNode.classList.add('circle');
+                    e.target.parentNode.style.pointerEvents = 'none';
+                    gameResult[X][Y] = 'circle';
+                    game.gameOver = verifResult();
+                    if(game.gameOver == true){
+                        containerAffichage.style.display = 'flex';
+                        affichage.innerText = `Bravo ${player1}, vous avez GAGNE !`;
+                        // console.table(gameResult);
+                    } else if (gameTurn.value > 7 && game.gameOver == false){
+                        containerAffichage.style.display = 'flex';
+                        affichage.innerText = `PERDU vous pouvez recommencer !`;
+                    };
+                    console.log(gameTurn.value);
+                    console.log(game.gameOver);
+                }
+                gameTurn.value++;
+            });
+        return table;
+    }
+    restart(){
+        grid.innerHTML = '';
+        containerAffichage.style.display = 'none';
+        
+        game = new Game(2, player1, player2);
+        gameGrid = game.createGrid(3, 3);
+        gameResult = gameGrid.map( el => el);
+        table = game.showGrid(gameGrid);
+        grid.appendChild(table);
+        gameTurn.value = 0;
+        game.gameOver = false;
+        game.nbParty++;
+    }
 }
